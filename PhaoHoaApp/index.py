8e49f689d27e74a428a05a72a2e7cfa8ef1359c6
PNamGP1120 <phuongnam.it0212@gmail.com>
2025-01-28 22:40:30 +0700

    phaohoa

---
+++ PhaoHoaApp/index.py
@@ -0,0 +1,9 @@
+from PhaoHoaApp import app
+from flask import render_template
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+if __name__ == '__main__':
+    app.run(port=5001, debug=True)
